039bf5f5439a4c1a47bed5db6b3052e75d80f8e7
Caio Vinicius <caio.moraes@fiap.com.br>
2024-09-26 01:00:22 -0300

    sincronizar namespaces

--- PolarisContacts.CreateService/Filters/AuthenticationFilter.cs
+++ PolarisContacts.CreateService/Filters/AuthenticationFilter.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace PolarisContacts.Filters
+namespace PolarisContacts.CreateService.Filters
 {
     public class AuthenticationFilterAttribute : ActionFilterAttribute
     {
